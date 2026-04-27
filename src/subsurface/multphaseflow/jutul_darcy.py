Fix sub-step bug for gradient

src/subsurface/multphaseflow/jutul_darcy.py
@@ -241,9 +241,9 @@ class JutulDarcy:
             actnum_vec = np.ones(nx*ny*nz)
 
         # Simulate and get results
-        jlres = julia.simulate_reservoir(case, info_level=-1)
+        jlres = julia.simulate_reservoir(case, info_level=-1, output_substates=True)
         pyres, daysIDX = self.extract_datatypes(jlres, case, units, julia)
-
+        
         # Convert output to requested format
         if not self.output_format == 'dataframe':
             if self.output_format == 'dict':
@@ -317,7 +317,8 @@ class JutulDarcy:
                 funcs = well_QOI_objective(
                     wellID=info['wellID'], 
                     phaseID=info['phase'], 
-                    stepID=stepIDX, 
+                    stepID=stepIDX,
+                    time=np.array(jlres.time),
                     rate=info['is_rate'], 
                     julia=julia
                 )
@@ -330,7 +331,7 @@ class JutulDarcy:
                 # Comute adjoint sensitivities
                 julia.case = case
                 julia.res = jlres
-                for func in funcs:
+                for i, func in enumerate(funcs):
                     julia.func = func
                     grad = julia.seval("""
                     redirect_stdout(devnull) do
@@ -346,7 +347,7 @@ class JutulDarcy:
                     # Evaluate functions
                     if self.eval_adjoint_funcs:
                         func_val = julia.Jutul.evaluate_objective(func, case, jlres.result)
-                        func_val = func_val*24*60*60 if info['is_rate'] else func_val
+                        func_val = func_val*julia.seval('si_unit(:day)') if info['is_rate'] else func_val
                         func_dict[col].append(func_val)
 
 
@@ -491,21 +492,19 @@ def _extract_adjoint(jlgrad, jlcase, parameter, actnum, is_rate, julia):
             adjoint = adjoint * perm.flatten(order='F')
             # Note: For dJ/dlog(perm) = dJ/dperm * perm, no need to convert from m2 to mD.
         else:
-            m2_per_mD = 9.86923000000e-16 # m2/mD
-            adjoint = adjoint * m2_per_mD
+            adjoint = adjoint * julia.seval('si_unit(:milli)*si_unit(:darcy)')
     else:
         raise ValueError(f"Adjoint not implemented for parameter '{parameter}'")
     
     if is_rate:
-            sec_per_day = 24*60*60
-            adjoint = adjoint * sec_per_day  # Convert from per sec to per day
+        adjoint = adjoint * julia.seval('si_unit(:day)')  # Convert from per sec to per day
 
     return adjoint
         
 
 def _active_to_full_grid(vec, actnum_vec, fill_value=0.0):
     if len(vec) == actnum_vec.sum():
-        full_vec = np.full(actnum_vec.shape, fill_value, dtype=np.float64)
+        full_vec = np.full(actnum_vec.shape, fill_value, dtype=np.float64, order='F')
         full_vec[actnum_vec == 1] = vec
         return full_vec
     if len(vec) == len(actnum_vec):
@@ -536,6 +535,7 @@ def _process_adjoint_info(adjoint_info):
         'WOPR': ('oil', True),
         'WGPR': ('gas', True),
         'WWPR': ('water', True),
+        'WWIR': ('water', True),
         'WLPR': ('liquid', True),
     }
     info = {}
@@ -585,7 +585,7 @@ def get_metric_unit(key: str) -> str:
     return unit_map.get(key.upper(), "Unknown")
 
 
-def well_QOI_objective(wellID, phaseID, stepID=None, rate=True, julia=None):
+def well_QOI_objective(wellID, phaseID, stepID=None, time=None, rate=True, julia=None):
     if julia is None:
         from juliacall import Main as julia
         julia.seval("using JutulDarcy")
@@ -635,7 +635,7 @@ def well_QOI_objective(wellID, phaseID, stepID=None, rate=True, julia=None):
             julia.seval(
                 f"""
                 function well_QOI_{sID}(model, state, dt, step_i, forces)
-                    if step_i[:step] != {sID+1}
+                    if (step_i[:step] != {sID+1}) && (step_i[:time] != {time[sID+1]})
                         return 0.0
                     else
                         ctrl = forces[:Facility].control[Symbol("{wellID}")]
@@ -664,7 +664,7 @@ def well_QOI_objective(wellID, phaseID, stepID=None, rate=True, julia=None):
     julia.seval(
         f"""
         function well_QOI(model, state, dt, step_i, forces)
-            if step_i[:step] != {stepID+1}
+            if (step_i[:step] != {stepID+1}) && (step_i[:time] != {time[stepID+1]})
                 return 0.0
             else
                 ctrl = forces[:Facility].control[Symbol("{wellID}")]